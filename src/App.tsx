d092c60ddfb947e3deb5dfa9544161c1e5b6bdfd
Estifanos Wendimu <teffsauce2@gmail.com>
2026-04-22 14:09:41 +0300

    alias update

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import gsap from 'gsap';
 import { ScrollSmoother, ScrollTrigger } from 'gsap/all';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
-import BenefitSection from  '@comps/sections/BenefitSection.tsx'
-import FlavorSection from './components/sections/FlavorsSection.tsx';
-import HeroSection from './components/sections/HeroSection.tsx';
-import MessageSection from './components/sections/MessageSection.tsx';
-import NutritionSection from './components/sections/NutritionSection.tsx';
-import TestimonialSection from './components/sections/TestimonialSection.tsx';
+import BenefitSection from  '@comps/sections/BenefitSection'
+import FlavorSection from '@comps/sections/FlavorsSection';
+import HeroSection from '@comps/sections/HeroSection';
+import MessageSection from '@comps/sections/MessageSection';
+import NutritionSection from '@comps/sections/NutritionSection';
+import TestimonialSection from '@comps/sections/TestimonialSection';
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 export default function App() {
