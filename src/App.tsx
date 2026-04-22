46078b393642213168890d0e10102dc486aae14f
Estifanos Wendimu <teffsauce2@gmail.com>
2026-04-22 13:59:00 +0300

    testing build fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import gsap from 'gsap';
 import { ScrollSmoother, ScrollTrigger } from 'gsap/all';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
-import BenefitSection from './components/sections/BenefitSection';
-import FlavorSection from './components/sections/FlavorsSection';
-import HeroSection from './components/sections/HeroSection';
-import MessageSection from './components/sections/MessageSection';
-import NutritionSection from './components/sections/NutritionSection';
-import TestimonialSection from './components/sections/TestimonialSection';
+import BenefitSection from './components/sections/BenefitSection.tsx';
+import FlavorSection from './components/sections/FlavorsSection.tsx';
+import HeroSection from './components/sections/HeroSection.tsx';
+import MessageSection from './components/sections/MessageSection.tsx';
+import NutritionSection from './components/sections/NutritionSection.tsx';
+import TestimonialSection from './components/sections/TestimonialSection.tsx';
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 export default function App() {
