c2775fba04338785a42a44f0acf5a76bf2792a2d
Estifanos Wendimu <teffsauce2@gmail.com>
2026-04-22 14:08:15 +0300

    testing build fix

--- src/components/Navbar.tsx
+++
@@ -1,7 +0,0 @@
-export default function Navbar() {
-  return (
-    <nav className="fixed top-0 z-50 p-3 md:p-9">
-      <img src="/images/nav-logo.svg" alt="Spylt logo" className="w-20 md:w-24" />
-    </nav>
-  );
-}
