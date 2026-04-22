21859aa0bed80c85760a6cdbf45853888639c3c6
Estifanos Wendimu <teffsauce2@gmail.com>
2026-04-22 20:58:32 +0300

    updated asset

--- src/components/VideoSection.tsx
+++ src/components/VideoSection.tsx
@@ -92,7 +92,7 @@ export default function VideoSection() {
       <div className="video-box size-full" style={{ clipPath: 'circle(6% at 50% 50%)' }}>
         <video src="/videos/pin-video.mp4" muted loop autoPlay playsInline />
         <div className="abs-center scale-200 md:scale-100">
-          <img src="/images/circle-text.svg" alt="circle text" className="animate-spin-circle" />
+          <img src="/images/optimized/chug-spylt.webp" alt="circle text" className="animate-spin-circle" />
           <div className="play-btn">
             <Play fill="white" color="white" className="ml-[.5vw] size-[3vw]" />
           </div>
